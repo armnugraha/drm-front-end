b9636fb32d0df190ceb2daf0f2a1503c5ea1e192
arman nugraha <armannugraha85@gmail.com>
2019-11-12 16:58:28 +0700

    modified data

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -30,6 +30,41 @@ class ProductsTableSeeder extends Seeder
             	'pcs_price' => 1800,
             	'dozen_price' => 50000
             ),
+            2 => 
+            array (
+                'name' => 'Sedap Kuah',
+            	'barcode' => '082140',
+            	'pcs_price' => 2200,
+            	'dozen_price' => 60000
+            ),
+            3 => 
+            array (
+                'name' => 'Sedap Goreng',
+            	'barcode' => '082141',
+            	'pcs_price' => 1900,
+            	'dozen_price' => 52000
+            ),
+            4 => 
+            array (
+                'name' => 'Cholotos',
+            	'barcode' => '082142',
+            	'pcs_price' => 1000,
+            	'dozen_price' => 50000
+            ),
+            5 => 
+            array (
+                'name' => 'Kopi ABC',
+            	'barcode' => '082143',
+            	'pcs_price' => 3800,
+            	'dozen_price' => 50000
+            ),
+            6 => 
+            array (
+                'name' => 'Larutan',
+            	'barcode' => '082144',
+            	'pcs_price' => 1800,
+            	'dozen_price' => 50000
+            ),
         ));
 
     }
